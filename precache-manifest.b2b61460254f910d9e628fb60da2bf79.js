ef2ad687c4cfc38c219eed60a8535f9a3d778f1f
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-07-20 22:44:23 +0530

    Updates

---
+++ precache-manifest.b2b61460254f910d9e628fb60da2bf79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0470d348b19354d51fc287da09f0e2e",
+    "url": "/pomodoro-react/index.html"
+  },
+  {
+    "revision": "021a71558cb3354aa5f8",
+    "url": "/pomodoro-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "33319eb6ed2a4911a968",
+    "url": "/pomodoro-react/static/css/main.19a2fe19.chunk.css"
+  },
+  {
+    "revision": "021a71558cb3354aa5f8",
+    "url": "/pomodoro-react/static/js/2.0ece43a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-react/static/js/2.0ece43a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33319eb6ed2a4911a968",
+    "url": "/pomodoro-react/static/js/main.90fce51b.chunk.js"
+  },
+  {
+    "revision": "e46251b4a667c98edcc0",
+    "url": "/pomodoro-react/static/js/runtime-main.353efe95.js"
+  }
+]);
